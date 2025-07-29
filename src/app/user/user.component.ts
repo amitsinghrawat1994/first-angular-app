5322677ca20e67ac89c802f10a71c355eb4a24f2
Amit Singh Rawat <amitsinghrawat99@gmail.com>
2025-07-29 12:20:04 +0530

    34. Working with Outputs & Emitting Data

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, input, signal } from '@angular/core';
+import { Component, computed, EventEmitter, Input, input, Output, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,10 +10,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({ required: true }) id!: string
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
   // avatar = input.required<string>();
   // name = input.required<string>();
+  @Output() select = new EventEmitter();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
@@ -21,6 +23,6 @@ export class UserComponent {
   // imagePath = computed(() => 'assets/users/' + this.avatar());
 
   onSelectUser() {
-
+    this.select.emit(this.id);
   }
 }
