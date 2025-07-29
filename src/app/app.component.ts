c42f6f07832473525abc3959a3572daccc40b75a
Amit Singh Rawat <amitsinghrawat99@gmail.com>
2025-07-29 19:36:32 +0530

    44. Adding More Components to the Demo App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ import { TasksComponent } from "./tasks/tasks.component";
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string = 'u1';
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId)
