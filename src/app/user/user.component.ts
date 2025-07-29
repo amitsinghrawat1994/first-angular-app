d0bd66d404c93ddb2f6d5299edc5df2605224a7b
Amit Singh Rawat <amitsinghrawat99@gmail.com>
2025-07-29 15:33:39 +0530

    40. TypeScript: Type Aliases & Interfaces

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -3,6 +3,18 @@ import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
+type User = {
+  id: string,
+  name: string,
+  avatar: string
+};
+
+// interface User {
+//   id: string,
+//   name: string,
+//   avatar: string
+// }
+
 @Component({
   selector: 'app-user',
   imports: [],
@@ -10,19 +22,22 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) id!: string
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+
+  @Input({ required: true }) user!: User;
+
   // avatar = input.required<string>();
   // name = input.required<string>();
-  @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
   // imagePath = computed(() => 'assets/users/' + this.avatar());
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
